f4adc7f3c373299817c328e1fa7b554f042f902a
avelinpv <avelinpv@kth.se>
2019-12-09 15:49:14 +0100

    working prototype with sounds

--- ARmemoryUnity/Assets/Scripts/audioPlay.cs
+++ ARmemoryUnity/Assets/Scripts/audioPlay.cs
@@ -3,32 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class audioPlay : MonoBehaviour
-{
-        // Start is called before the first frame update
-
-    public GameObject canvas;
-
+{    
+    public Transform target;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-        RaycastHit rayhit = new RaycastHit();
+        AudioSource audio = GetComponent<AudioSource>();
+        float distToTarget = Vector3.Distance(transform.position, target.position);
 
-        if (Physics.Raycast(ray, out rayhit)){
-            GameObject hitObject = rayhit.collider.gameObject;
-            //AudioSource sound = hitObject.GetComponent<AudioSource>();
-
-            canvas.SetActive(true);
-            UnityEngine.UI.Button btn = UnityEngine.UI.Button.FindWithTag("button");
-
-            btn.onClick.AddListener(delegate() { 
-                hitObject.GetComponent<AudioSource>().Play();
-            });
+        if(audio.isPlaying && distToTarget < 1){
+            audio.mute = false;
+            audio.volume = 1/distToTarget;
+        } else if(distToTarget < 1){
+            audio.mute = false;
+            audio.Play();
+        }else {
+            audio.Stop();
         }
+
     }
 }
